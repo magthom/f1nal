9887e65d15d8bc38c83216a643266ff7f124b94c
magthom <61918059+magthom@users.noreply.github.com>
2020-08-03 10:15:50 -0500

    Update server.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -10,11 +10,7 @@ app.use(express.json());
 
 app.use('/api', apiRouter);
 
-<<<<<<< HEAD
 //app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/')));
-
-=======
->>>>>>> 3ce01ba2e2b4a10491e9b7019dfc6b8db7d357bd
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
