4aaae1f35954e00e063fb7062e883a684b9b9ed0
magthom <61918059+magthom@users.noreply.github.com>
2020-08-03 14:22:41 -0500

    Update charities.tsx

--- src/client/views/charities.tsx
+++ src/client/views/charities.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { ICharity } from '../UTILS/types';
+import OrgCard from '../components/OrgCard';
 
 class Charities extends React.Component<IcharityProps, IcharityState> {
 	constructor(props: IcharityProps) {
@@ -9,9 +10,10 @@ class Charities extends React.Component<IcharityProps, IcharityState> {
 
 	async componentDidMount() {
 		try {
-			let r = await fetch('/api/charities/get');
+			let r = await fetch('/api/charities');
 			let charities = await r.json();
 			this.setState({ charities });
+		
 		} catch (error) {
 			console.log(error);
 		}
@@ -22,21 +24,17 @@ class Charities extends React.Component<IcharityProps, IcharityState> {
 			<main className="container">
 				<h1 className="text-primary text-center">Charities</h1>
 				<ul className="list-group"> 
-					{this.state.charities.map(charity =>{ 
-						return <li key={charity.id} className="list-group-item">{charity.charity_name}
-						</li>
-					})}
+					{this.state.charities.map(charity => <OrgCard charity={charity} />)}
 				</ul>
 			</main>
 		);
 	}
-	
 }
 
 export interface IcharityProps {}
 
 export interface IcharityState {
-	charities: Array<ICharity>
+	charities: Array<ICharity>;
 }
 
 export default Charities;
