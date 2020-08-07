e4060e8b9fd69f0edff35b0cefe59ea7c392515f
magthom <61918059+magthom@users.noreply.github.com>
2020-08-07 09:39:14 -0500

    Update Details.tsx

--- src/client/views/Details.tsx
+++ src/client/views/Details.tsx
@@ -3,6 +3,7 @@ import { ICharity } from "../UTILS/types";
 import { match } from "react-router";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
+import "../scss/details.scss";
 
 class Details extends React.Component<DetailsProps, { org: any }> {
   id: any;
@@ -31,49 +32,43 @@ class Details extends React.Component<DetailsProps, { org: any }> {
   }
   render() {
     return (
-      <div>
+
+      <div className="detailsContainer">
         <Navbar />
         <div className="banner">
         </div>
-        <div id="wrapper">
-          <div id="content">
+        <div className="wrapper">
+          <div className="content">
             <div className="main">
-              <h1 id="charity_name">{this.state.org.charity_name}</h1>
+              <h1 className="charity_name">{this.state.org.charity_name}</h1>
               <div className="mission">
-                <button>Donate to this charity</button>
+                <button className="btn btn-outline-primary"><a href={this.state.org.websiteURL} styles="text-decoration: none; background-color: ;"> More information / Donate</a></button>
                 <h3>Mission</h3>
-                <p>{this.state.org.mission}</p>
+                <br />
+                <h2> {this.state.org.tagline} </h2>
+                <p className="missionStatement shadow">{this.state.org.mission}</p>
               </div>
             </div>
-            <div id="contact">
+            <div className="contact">
               <h3>Charity Contact Info</h3>
               <form
-                id="address"
+                className="Address"
               >
-                Address:
+                Address: {this.state.org.city} {" AL, "} {this.state.org.streetAddress1} {", "} {this.state.org.postal_code}
+              </form>
+
+
+              <form
+                className="ein">
+                EIN: {this.state.org.EIN}
               </form>
               <form
-                id="phone"
-              >
-                Phone:
+                className="website">
+                Website: {this.state.org.websiteURL}
               </form>
-              <form
-                id="fax"
-              >
-                Fax:
-              </form>
-              <form
-                id="ein">
-                EIN:
-              </form>
-              <form
-                id="website">
-                Website:
-              </form>
-              <div id="rating">
-                <h3>Rating</h3>
-                <p></p>
-              </div>
+              {/* <form className="rating">
+                  Rating: {this.state.org.rating}
+              </form> */}
             </div>
           </div>
         </div>
