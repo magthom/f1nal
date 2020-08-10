f25cf67c9d8a37e154cf3172e3e30852d718a500
magthom <61918059+magthom@users.noreply.github.com>
2020-08-10 12:33:15 -0500

    new changes

--- src/client/views/charities.tsx
+++ src/client/views/charities.tsx
@@ -4,24 +4,19 @@ import OrgCard from '../components/OrgCard';
 import Navbar from './Navbar';
 import '../scss/charities.scss';
 
-
 class Charities extends React.Component<IcharityProps, IcharityState> {
 	constructor(props: IcharityProps) {
 		super(props);
-		this.state = { 
-			charities: [],
-			filteredCharities: []
-		  };
-		
+		this.state = { charities: [], 
+			filteredCharities: [],
+            filtered: false};
 	}
 
 	async componentDidMount() {
 		try {
-			
 			let r = await fetch('/api/charities');
 			let charities = await r.json();
 			this.setState({ charities });
-			console.log(charities)
 
 		} catch (error) {
 			console.log(error);
@@ -29,63 +24,124 @@ class Charities extends React.Component<IcharityProps, IcharityState> {
 	}
 
 	filterResults(category) {
-	
-	}
-	
-	
-	render() {
-		
-		return (
-			<>
-				<div>
+		let filterArr = []
+		console.log ("something");
+    for (let index = 0; index < this.state.charities.length; index++) {
+        if (this.state.charities[index].category_name == category) {
+			filterArr.push(this.state.charities[index])
+			console.log (filterArr);
+          this.setState({ filteredCharities: filterArr })
+        }   
+    }
+    }
 
-					<title>Document</title>
-					<Navbar />
-					<div className="charityWrapper">
-						<div className="charityBorder">
-							<div className="charityContent">
-								<div className="charityNavLeft">
-									<div className="directoryBackground">
-										<h1 className="directory"> Charity Directory</h1>
-									</div>
-		<p className="animals" onClick={() => this.filterResults("Animals")}> Animals </p>
-									<p className="arts">Arts </p>
-									<p className="culture"> Culture </p>
-									<p className="humanities"> Humanities </p>
-									<p className="commDev">  Community Development </p>
-									<p className="education"> Education </p>
-									<p className="health"> Health </p>
-									<p className="humanServices"> Human Services </p>
-									<p className="international"> International </p>
-									<p className="rights"> Human and Civil Rights </p>
-									<p className="religion"> Religion </p>
-									<p className="research"> Research and Public Policy </p>
-								</div>
-								<div className="containerRed">
-								<main className="container">
+	render() {
+        if (this.state.filtered == false) {return (
+            <>
+                <div>
+                    <title>Document</title>
+                   
+                    <div className="charityWrapper">
+                        <div className="charityBorder">
+                            <div className="charityContent">
+                                <div className="charityNavLeft">
+                                    <div className="directoryBackground">
+                                        <h1 className="directory"> Charity Directory</h1>
+                                    </div>
+									<p className="all" onClick={()=>{this.setState({filtered:false})}}> All</p>
+                                    <p className="animals" onClick={() => {this.filterResults("Animals")
+                                this.setState({filtered: true})}}> Animals </p>
+                                    <p className="arts" onClick={() => {this.filterResults("Arts, Culture, Humanities")
+                                this.setState({filtered: true})}}>Arts, Cultures, and Humanities </p>
+                                
+                                    <p className="commDev" onClick={() => {this.filterResults("Community Development")
+                                this.setState({filtered: true})}}> Community Development </p>
+                                    <p className="education" onClick={() => {this.filterResults("Education")
+                                this.setState({filtered: true})}}> Education </p>
+                                    <p className="health" onClick={() => {this.filterResults("Health")
+                                this.setState({filtered: true})}}> Health </p>
+                                    <p className="humanServices" onClick={() => {this.filterResults("Human Services")
+                                this.setState({filtered: true})}}> Human Services </p>
+                                    <p className="international" onClick={() => {this.filterResults("International")
+                                this.setState({filtered: true})}}> International </p>
+                                    <p className="rights" onClick={() => {this.filterResults("Human and Civil Rights")
+                                this.setState({filtered: true})}}> Human and Civil Rights </p>
+                                    <p className="religion" onClick={() => {this.filterResults("Religion")
+                                this.setState({filtered: true})}}> Religion </p>
+                                    <p className="research" onClick={() => {this.filterResults("Research and Public Policy")
+                                this.setState({filtered: true})}}> Research and Public Policy </p>
+                                </div>
+                                <div className="containerRed">
+                                <main className="container">
                 <h1 className="text-primary text-center">Charities</h1>
                 <ul className="list-group"> 
                     {this.state.charities.map(charity => <OrgCard charity={charity} />)}
                 </ul>
             </main>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</>
-		);
-	}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+    else if (this.state.filtered == true) {return (
+        <>
+        <div>
+            <title>Document</title>
+           
+            <div className="charityWrapper">
+                <div className="charityBorder">
+                    <div className="charityContent">
+                        <div className="charityNavLeft">
+                            <div className="directoryBackground">
+                                <h1 className="directory"> Charity Directory</h1>
+                            </div>
+							<p className="all" onClick={()=>{this.setState({filtered:false})}}> All</p>
+							<p className="animals" onClick={() => {this.filterResults("Animals")
+                                this.setState({filtered: true})}}> Animals </p>
+                                    <p className="arts" onClick={() => {this.filterResults("Arts, Culture, Humanities")
+                                this.setState({filtered: true})}}>Arts, Cultures, and Humanities </p>
+                                
+                                    <p className="commDev" onClick={() => {this.filterResults("Community Development")
+                                this.setState({filtered: true})}}> Community Development </p>
+                                    <p className="education" onClick={() => {this.filterResults("Education")
+                                this.setState({filtered: true})}}> Education </p>
+                                    <p className="health" onClick={() => {this.filterResults("Health")
+                                this.setState({filtered: true})}}> Health </p>
+                                    <p className="humanServices" onClick={() => {this.filterResults("Human Services")
+                                this.setState({filtered: true})}}> Human Services </p>
+                                    <p className="international" onClick={() => {this.filterResults("International")
+                                this.setState({filtered: true})}}> International </p>
+                                    <p className="rights" onClick={() => {this.filterResults("Human and Civil Rights")
+                                this.setState({filtered: true})}}> Human and Civil Rights </p>
+                                    <p className="religion" onClick={() => {this.filterResults("Religion")
+                                this.setState({filtered: true})}}> Religion </p>
+                                    <p className="research" onClick={() => {this.filterResults("Research and Public Policy")
+                                this.setState({filtered: true})}}> Research and Public Policy </p>
+                        </div>
+                        <div className="containerRed">
+                        <main className="container">
+        <h1 className="text-primary text-center">Charities</h1>
+        <ul className="list-group"> 
+            {this.state.filteredCharities.map(charity => <OrgCard charity={charity} />)}
+        </ul>
+    </main>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+);
+    }}
 }
 
-export interface IcharityProps {
-	charities: Array<ICharity>;
-	
- }
+export interface IcharityProps { }
 
 export interface IcharityState {
 	charities: Array<ICharity>;
-
 }
 
 export default Charities;
