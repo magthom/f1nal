9c15cb8aa0095b1b077cc80664a969feb4d53cee
magthom <61918059+magthom@users.noreply.github.com>
2020-08-13 09:59:45 -0500

    up2date

--- src/client/views/Navbar.tsx
+++ src/client/views/Navbar.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect }  from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import '../scss/home.scss';
-
-const Navbar: React.FC<{}> = () => {
+const Navbar: React.FC<{}> = (props) => {
+    let history = useHistory() 
+    const [charities, setCharities] = useState([])
+    const [search, setSearch] = useState("")
+    const checkCharities = () => {
+        for(let i=0 ; i<charities.length ; i++){
+            if (charities[i].charity_name == search) {
+                console.log(charities[i].charity_name)
+                history.push(`/details/${charities[i].id}`)
+            }
+        }
+    }
+    useEffect(() => {
+        (async ()=> {
+            let r = await fetch("/api/charities");
+            let charities = await r.json();
+            setCharities( charities );
+        })()
+    }, [])
     return (
         <>
-       
             <div className="top">
                 <div className="logo">
-                    <img src="./assests/redlogo2.jpg" alt="Helping Hands Logo" width={300} className="auto-style5" height={70} />
+                    <img src="./assests/logo3.jpg" alt="Helping Hands Logo" width={350} className="auto-style5" height={60} />
                 </div>
                 <div className="social-media">
                     <p>Login</p>
@@ -35,12 +51,11 @@ const Navbar: React.FC<{}> = () => {
                     <li className="homeName"><Link to="/">Home</Link></li>
                     <li><Link to="/charities">Charities</Link></li>
                     <li><Link to="/about">About</Link></li>
-                    <input className="search" type="text" placeholder="Search for charities.." />
+                    <input className="search" type="text" value={search} onChange={(e) => {setSearch(e.target.value)} } placeholder="Search for charities.." />
+                    <button className="searchbtn btn btn-primary" onClick={() => {checkCharities()}}>search</button>
                 </ul>
             </div>
-           
         </>
     )
 }
-
 export default Navbar
