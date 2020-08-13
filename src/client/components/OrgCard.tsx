15750f2189bf521313183d0b71848200c63c7075
Chastonharper <64808674+Chastonharper@users.noreply.github.com>
2020-08-13 09:49:28 -0500

    Add files via upload

--- src/client/components/OrgCard.tsx
+++ src/client/components/OrgCard.tsx
@@ -1,19 +1,22 @@
 import * as React from "react";
 import { ICharity } from "../utils/types";
 import { useHistory, useParams } from "react-router-dom";
+import '../scss/charities.scss';
+
+
 
 const OrgCard: React.FC<OrgCardProps> = (props) => {
 
   const history = useHistory()
   console.log(props)
   return (
-      <div className="col-md-6 mx-1">
+      <div className="col-md-6 mx-1 hvr-grow orgCard">
         <div
           onClick={() => history.push(`/details/${props.charity.id}`)}
           className="card my-2 shadow">
           <div className="card-body text-center">
             <h4 className="card-title">{props.charity.charity_name}</h4>
-            <p className="card-text">{props.charity.category_name}</p>
+            <p className="card-text">{props.charity.cause_name}</p>
           </div>
         </div>
       </div>
